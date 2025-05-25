bede6d5fc33aec605e26fcaabefd20c73158d8b7
SaikatDeb12 <saikat.rcld@gmail.com>
2025-05-25 22:44:09 +0530

    creating schema

---
+++ server/model/user.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(this.password, password);
+};
+
+export const User = model("users", userSchema);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import express, { json } from "express";
-import mongoose from "mongoose";
 const app = express();
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,3 +8,6 @@ dotenv.config();
 connectDB();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.listen(process.env.PORT, () =>
+  console.log(`Server started at PORT ${process.env.PORT}`)
+);
