6b900957e2aa68ab67172d7174b08b73b81c1aa7
SaikatDeb12 <saikat.rcld@gmail.com>
2025-05-26 14:40:02 +0530

    styling signup page

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,13 +1,14 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import "./register.css";
 const Register: React.FC = () => {
   const schema = z.object({
     name: z.string().min(1, "Required"),
     email: z.string().email("Invalid email"),
     password: z.string().min(4, "Min. characters should be atleast 4"),
   });
+
   const {
     register,
     handleSubmit,
@@ -17,18 +18,47 @@ const Register: React.FC = () => {
   const onSubmit = () => {};
 
   return (
-    <div className="h-screen flex items-center justify-center gap-4 m-3">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="name" placeholder="Enter name" {...register("name")} />
-        <p>{errors && errors.name?.message}</p>
-        <input type="email" placeholder="Enter email" {...register("email")} />
-        <p>{errors && errors.email?.message}</p>
+    <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <label className="block text-sm font-medium text-gray-700">Name</label>
         <input
+          className="w-full p-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          type="text"
+          placeholder="Enter name"
+          {...register("name")}
+        />
+        <p className="text-red-500 text-center mb-4">
+          {errors && errors.email?.message}
+        </p>
+        <label className="block text-sm font-medium text-gray-700">Email</label>
+        <input
+          className="w-full p-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          type="email"
+          placeholder="Enter email"
+          {...register("email")}
+        />
+        <p className="text-red-500 text-center mb-4">
+          {errors && errors.email?.message}
+        </p>
+        <label className="block text-sm font-medium text-gray-700">
+          Password
+        </label>
+        <input
+          className="w-full p-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="password"
           placeholder="Enter password"
           {...register("password")}
         />
-        <p>{errors && errors.password?.message}</p>
+        <p className="text-red-500 text-center mb-4">
+          {errors && errors.password?.message}
+        </p>
+        <button
+          className="border p-1 rounded-md w-full bg-blue-500 text-white hover:opacity-90"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
